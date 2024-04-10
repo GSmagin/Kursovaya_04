bb1ec13fdf19f7da90d9576ae4893bd5321798bf
Rocco <grog512new@mail.ru>
2024-04-11 01:17:09 +0300

    создание экземпляра вакансий

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.class_api import *
 from src.class_file import ClassFile
-
+from src.class_vacancies_hh import VacanciesHH
 
 def main():
 
@@ -13,12 +13,35 @@ def main():
     vacancy = ClassAPIHH().get_api(name_vacancy, name_region)
     # сохранение в файл результата запроса
     ClassFile.save_to_file(vacancy)
+    instance_vacancy = []
+    for i in ClassFile.load_from_file():
+        instance_vacancy.append(VacanciesHH(
+            i.get('name'),  # Вакансия
+            i.get('area').get('name'),  # Регион
+            i.get('salary').get('from'),  # Зарплата от
+            i.get('salary').get('to'),  # Зарплата до
+            i.get('salary').get('currency'),  # Валюта зарплаты
+            i.get('snippet').get('requirement'),  # Комментарий
+            i.get('snippet').get('responsibility'),  # Обязанности
+            i.get('employer').get('vacancies_url'))  # Ссылка на вакансию
+        )
+
+
+
+        #print(f"{i.get('area').get('name')}, {i.get('name')}")
+
+    print(instance_vacancy[1].__dict__)
+
+
+
+
 
 
 
     # print(ClassFile.load_from_file().get("items"))
-    for i in ClassFile.load_from_file():
-        print(f"{i.get('area').get('name')}, {i.get('name')}")
+    # for i in ClassFile.load_from_file():
+    #     print(f"{i.get('area').get('name')}, {i.get('name')}")
+    # print(f"количество вакансий {len(vacancy)}")  # количество вакансий
 
 
 if __name__ == "__main__":
--- src/class_vacancies_hh.py
+++ src/class_vacancies_hh.py
@@ -1,7 +1,38 @@
+
 class VacanciesHH:
+    """
+    Класс для представления вакансий
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, name, area_name, salary_from, salary_to, salary_currency,
+                 snippet_requirement, snippet_responsibility, url):
+        """
+        :param name Вакансия
+        :param area_name Регион
+        :param salary_from Зарплата от
+        :param salary_to Зарплата до
+        :param salary_currency Валюта зарплаты
+        :param snippet_requirement Комментарий
+        :param snippet_responsibility Обязанности
+        :param url Ссылка на вакансию
+        """
 
+        self.name = name
+        self.area_name = area_name
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.salary_currency = salary_currency
+        self.snippet_requirement = snippet_requirement
+        self.snippet_responsibility = snippet_responsibility
+        self.url = url
 
+        self.validate_salary()
+
+    def validate_salary(self):
+        """
+        Валидация данный о вакансии
+        :return: Если зарплата не указана, устанавливает значение 0 для salary_from.
+        """
+        if not self.salary_from and not self.salary_to:
+            self.salary_from = 0
 
