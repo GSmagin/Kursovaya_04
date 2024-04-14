f4665abf24fd5f93987c9c06420099d15f977516
Rocco <grog512new@mail.ru>
2024-04-14 06:29:44 +0300

    Добавлено сортировка выборка сохранение

--- src/class_api.py
+++ src/class_api.py
@@ -1,15 +1,24 @@
+from typing import List, Any, Dict
+
 import requests
-from abc import ABC, abstractmethod
-from confing import DIR_JSON_VACANCIES, DIR_JSON_VACANCIES_SORT
 
 
-class ClassAPI(ABC):
-    @abstractmethod
-    def get_api(self, word_search) -> dict:
-        pass
+# from abc import ABC, abstractmethod
+# from confing import DIR_JSON_VACANCIES, DIR_JSON_VACANCIES_SORT
+#
+#
+# class ClassAPI(ABC):
+#     @abstractmethod
+#     def get_api(self, word_search) -> dict:
+#         pass
+#
+#     @abstractmethod
+#     def get_api_2(self, word_search, region, sort) -> dict:
+#         pass
+#
 
 
-class ClassAPIHH(ClassAPI):
+class ClassAPIHH:
     """Подключается к api.hh.ru и получает вакансии по ключевому слову"""
 
     def get_api(self, word_search, region) -> dict:
@@ -24,16 +33,44 @@ class ClassAPIHH(ClassAPI):
         params = {
             'text': word_search,  # Ключевое слово
             'area': region,  # Регион
-            # 'per_page': 100,  # количество результатов на странице;
-            # 'page': 2,  # номер страницы
+            'per_page': 100,  # количество результатов на странице;
+            'page': 1,  # номер страницы
             # 'order_by':  order_by,  # Сортировка по зарплате
             'only_with_salary': 'true'  # Возвращать только вакансии с заработной платой
         }
         response = requests.get(url, params=params)
         return response.json()
 
+    def get_api_2(self, word_search, region, num_pages=1) -> dict[str, list[Any]]:
+        """
+        Получает вакансии по ключевому слову из API сервиса hh.ru
+        :param word_search: Ключевое слово для поиска вакансий
+        :param region: Регион
+        :param num_pages: Количество страниц для загрузки
+        :return: JSON-данные с информацией о вакансиях
+        """
+        url = 'https://api.hh.ru/vacancies'
+        all_vacancies = {"items": []}
 
-class ClassAPIHHR(ClassAPI):
+        for page_num in range(1, num_pages + 1):
+            params = {
+                'text': word_search,  # Ключевое слово
+                'area': region,  # Регион
+                'per_page': 100,  # количество результатов на странице;
+                'page': page_num,  # номер страницы
+                'only_with_salary': 'true'  # Возвращать только вакансии с заработной платой
+            }
+            response = requests.get(url, params=params)
+            if response.status_code == 200:
+                vacancies_data = response.json()
+                all_vacancies["items"].extend(vacancies_data['items'])
+            else:
+                print(f"Failed to fetch data from page {page_num}")
+
+        return all_vacancies
+
+
+class ClassAPIHHR:
     """Подключается к api.hh.ru и получает id регионов"""
 
     def get_api(self, side="113") -> dict:
@@ -43,35 +80,3 @@ class ClassAPIHHR(ClassAPI):
             print(f"id={i.get('id')} {i.get('name')} ")
 
         return response.json()
-
-# class ClassAPIHHSort(ClassAPI):
-#     def get_api(self, vacancy_search_order="vacancy_search_order"):
-#         url = 'https://api.hh.ru/dictionaries'
-#         response = requests.get(url)
-#         for i in response.json().get(vacancy_search_order):
-#             print(f"id={i.get('id')} {i.get('name')} ")
-#
-#         return response.json()
-#
-#         # list_vacancy_search_order = []
-#         # number = 1
-#         # for i in response.json().get('vacancy_search_order'):
-#         #     i["Number"] = number
-#         #     list_vacancy_search_order.append(i)
-#         #     print(f"{i.get('Number')} {i.get('name')}")
-#         #     number += 1
-#         # return list_vacancy_search_order
-
-
-# rt = input("Введите слово для поиска ")
-#
-# ClassAPIHHR().get_api()
-# id = input("Введите id региона ")
-
-# ClassAPIHHSort().get_api()
-# vacancy_search_order = input("Введите id сортировки ")
-
-
-#
-# dir_json = ClassAPIHH().get_api(rt, id)
-# print(ClassAPIHH().get_api(rt, id))
--- src/class_file.py
+++ src/class_file.py
@@ -1,4 +1,5 @@
-from confing import DIR_JSON_VACANCIES, DIR_JSON_VACANCIES_SORT
+import os
+from confing import DIR_JSON_VACANCIES, DIR_JSON_VACANCIES_SORT ,DIR_JSON_VACANCIES_SORT_TXT
 import json
 
 
@@ -12,10 +13,30 @@ class ClassFile:
             f.write(vacancies_json)
 
     @staticmethod
+    def save_to_file_txt(vacancies, filename=DIR_JSON_VACANCIES_SORT_TXT):
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(str(vacancies))
+
+    @staticmethod
+    def save_to_file_append(vacancies, filename=DIR_JSON_VACANCIES):
+        if not os.path.exists(filename):
+            with open(filename, 'w', encoding='utf-8') as f:
+                vacancies_json = json.dumps(vacancies, ensure_ascii=False, indent=4)
+                f.write(vacancies_json)
+        else:
+            with open(filename, "a", encoding="utf8") as f:
+                vacancies_json = json.dumps(vacancies, ensure_ascii=False, indent=4)
+                f.write(vacancies_json)
+
+    @staticmethod
     def load_from_file(filename=DIR_JSON_VACANCIES) -> list:
         with open(filename, "r", encoding="utf8") as f:
             vacancies = json.load(f)
         return vacancies
 
-    def delete_from_file(self, filename=DIR_JSON_VACANCIES_SORT) -> None:
-        pass
+    @staticmethod
+    def delete_from_file(filename=DIR_JSON_VACANCIES) -> None:
+        if os.path.exists(filename):
+            os.remove(filename)
+
+
